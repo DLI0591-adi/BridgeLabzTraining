2953dd5c30209be3a8b89824f9150829eee28ab8
Adi Aggarwal <Adi.Aggarwal@godigit.com>
2024-08-05 15:08:38 +0530

    [ADI] : Adding assignment 2 programs

---
+++ src/Day2/Logic_fourth.java
@@ -0,0 +1,15 @@
+package Day2;
+
+public class Logic_fourth {
+    public static void main(String[] args) {
+        int num = 1234; // Replace with your desired number
+        int reversed = 0;
+        System.out.println("Original Number: " + num);
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+        System.out.println("Reversed Number: " + reversed);
+    }
+}
